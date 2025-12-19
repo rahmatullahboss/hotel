6e54326b2461a16d2327688ab624a888d9ee36cf
rahmatullahboss <rahmatullahzisan01@gmail.com>
2025-12-19 23:10:29 +0600

    chore: update lockfile and cleanup db index

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -15,7 +15,6 @@ const createClient = () => {
     }
 
     const databaseUrl = process.env.DATABASE_URL;
-
     if (!databaseUrl) {
         throw new Error("DATABASE_URL environment variable is not set");
     }
